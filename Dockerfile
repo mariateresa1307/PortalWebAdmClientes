b1a3c84a4626256cd189f24ffea5225e4d491297
mariateresa1307 <mariateresa1307@gmail.com>
2024-08-09 17:23:52 -0400

    build fix

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ WORKDIR /app
 COPY . ./
 CMD npm ci
 CMD npm run build
+CMD npm run start
 
 
-
-FROM nginx:alpine
-COPY --from=build /app/out /usr/share/nginx/html
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
+#FROM nginx:alpine
+#COPY --from=build /app/out /usr/share/nginx/html
+#EXPOSE 80
+#CMD ["nginx", "-g", "daemon off;"]
--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,13 @@
 
-import  withPlugins from 'next-compose-plugins';
-import  optimizedImages from 'next-optimized-images';
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    output: "export",
-   
-    images: {
-        formats: ['image/avif'],
-
-      },
+    //output: process.env.NEXT_NODE_ENV === "dev" ? "standalone" : "export" ,
     typescript: {
 
         ignoreBuildErrors: true,
     }
 };
 
-export default withPlugins([
-    [optimizedImages],
-  
-    nextConfig
-  
-  ]);;
-
+export default nextConfig
 
